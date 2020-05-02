7c5dffaf0b03fa1471132b175e715c68b9800a54
guptaprakhariitr <prakharpguptag@gmail.com>
2020-05-02 21:12:50 +0530

    initial commit

---
+++ client.cpp
@@ -0,0 +1,51 @@
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <iostream>
+using namespace std;
+#define PORT 8888
+
+int main(int argc, char const *argv[]) 
+{ 
+    struct sockaddr_in address; 
+    int sock = 0, valread; 
+    struct sockaddr_in serv_addr; 
+    const char *hello = "Hello from client"; 
+    char buffer[1024] = {0}; 
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) 
+    { 
+        printf("\n Socket creation error \n"); 
+        return -1; 
+    } 
+   
+    memset(&serv_addr, '0', sizeof(serv_addr)); 
+   
+    serv_addr.sin_family = AF_INET; 
+    serv_addr.sin_port = htons(PORT); 
+       
+    // Convert IPv4 and IPv6 addresses from text to binary form 
+    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0)  
+    { 
+        printf("\nInvalid address/ Address not supported \n"); 
+        return -1; 
+    } 
+   
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) 
+    { 
+        printf("\nConnection Failed \n"); 
+        return -1; 
+    }
+    puts("Type message\n"); 
+  while(true){
+    char *msg=new char[4001];
+    printf("client - ");
+    cin.getline(msg,4000);
+    send(sock , msg , strlen(msg) , 0 ); 
+    valread = read( sock , buffer, 1024); 
+    printf("Server - %s\n",buffer ); }
+    return 0; 
+} 
